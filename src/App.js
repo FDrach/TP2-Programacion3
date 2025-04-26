d8383968f5d2b6983a0d5aa07e3bdc6eec70769a
Franco Drachenberg <francodrach@gmail.com>
2025-04-26 17:04:42 -0300

    Crear array de integrantes con datos placeholder on App.jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+Franco Eduardo Drachenberg - 61236
+Said Ghisaura - 00000
+Cristian Ballespin - 00000
+Ignacio Diaz Valdez - 00000
+Martin Alvarez - 00000
+*/
+
+import React from "react";
+import "./styles/App.css";
+import HomePages from "./pages/HomePages.jsx";
 
 function App() {
+  const integrantes = [
+    {
+      nombre: "Franco Eduardo",
+      apellido: "Drachenberg",
+      legajo: "61236",
+      github: "https://github.com/FDrach",
+      foto: "https://avatars.githubusercontent.com/FDrach",
+    },
+    {
+      nombre: "Said",
+      apellido: "Ghisaura",
+      legajo: "00000",
+      github: "https://github.com/sghisaura",
+      foto: "https://avatars.githubusercontent.com/github",
+    },
+    {
+      nombre: "Cristian",
+      apellido: "Ballespin",
+      legajo: "00000",
+      github: "https://github.com/cballespin",
+      foto: "https://avatars.githubusercontent.com/github",
+    },
+    {
+      nombre: "Ignacio",
+      apellido: "Diaz Valdez",
+      legajo: "00000",
+      github: "https://github.com/idiazvaldez",
+      foto: "https://avatars.githubusercontent.com/github",
+    },
+    {
+      nombre: "Martin",
+      apellido: "Alvarez",
+      legajo: "00000",
+      github: "https://github.com/AA-Martin07",
+      foto: "https://avatars.githubusercontent.com/AA-Martin07",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HomePages integrantesData={integrantes} />
     </div>
   );
 }
